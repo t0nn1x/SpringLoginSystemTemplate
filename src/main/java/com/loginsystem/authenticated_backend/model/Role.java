39c3f52aef6fdec093ac4b3f45f9f0b72d2a5125
t0nn1x <antoonhrobust@gmail.com>
2023-09-09 23:11:22 +0200

    Setted up basic SecurityConfig and created Role model

---
+++ src/main/java/com/loginsystem/authenticated_backend/model/Role.java
@@ -0,0 +1,54 @@
+package com.loginsystem.authenticated_backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    private String authority;
+
+
+    public Role() {
+        super();
+    }
+
+    public Role(String authority) {
+        this.authority = authority;
+    }
+
+    public Role(Integer roleId, String authority) {
+        this.roleId = roleId;
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+    
+    public void setAuthority(String authority){
+        this.authority = authority;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+}
