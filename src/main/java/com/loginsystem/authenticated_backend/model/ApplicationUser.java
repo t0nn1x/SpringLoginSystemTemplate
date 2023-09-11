317ff5ac42b6ff9462555950a03d357bb6ca9c8e
t0nn1x <antoonhrobust@gmail.com>
2023-09-11 19:32:17 +0200

    Created the AuthenticationService and registerUser method

--- src/main/java/com/loginsystem/authenticated_backend/model/ApplicationUser.java
+++ src/main/java/com/loginsystem/authenticated_backend/model/ApplicationUser.java
@@ -27,6 +27,7 @@ public class ApplicationUser implements UserDetails {
     @Column(name = "user_id")
     private Integer id;
 
+    @Column(unique = true)
     private String username;
 
     private String password;
---
+++ src/main/java/com/loginsystem/authenticated_backend/service/AuthenticationService.java
@@ -0,0 +1,40 @@
+package com.loginsystem.authenticated_backend.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.loginsystem.authenticated_backend.model.ApplicationUser;
+import com.loginsystem.authenticated_backend.model.Role;
+import com.loginsystem.authenticated_backend.repository.RoleRepository;
+import com.loginsystem.authenticated_backend.repository.UserRepository;
+
+@Service
+@Transactional
+public class AuthenticationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public ApplicationUser registerUser(String username, String password){
+        
+        String encodedPassword = passwordEncoder.encode(password);
+        Role userRole = roleRepository.findByAuthority("USER").get();
+
+        Set<Role> authorities = new HashSet<>();
+
+        authorities.add(userRole);
+
+        return userRepository.save(new ApplicationUser(0, username, encodedPassword, authorities));
+    }
+}
