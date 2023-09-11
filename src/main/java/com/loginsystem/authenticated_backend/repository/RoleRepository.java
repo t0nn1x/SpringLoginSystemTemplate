2c4ea55fb40c476c1c4439d17953608c182601fb
t0nn1x <antoonhrobust@gmail.com>
2023-09-11 18:57:28 +0200

    Loading Roles and Admin User

--- src/main/java/com/loginsystem/authenticated_backend/repository/RoleRepository.java
+++ src/main/java/com/loginsystem/authenticated_backend/repository/RoleRepository.java
@@ -3,9 +3,11 @@ package com.loginsystem.authenticated_backend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.loginsystem.authenticated_backend.model.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer>{
     Optional<Role> findByAuthority(String authority);
 }
--- src/main/java/com/loginsystem/authenticated_backend/repository/UserRepository.java
+++ src/main/java/com/loginsystem/authenticated_backend/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.loginsystem.authenticated_backend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.loginsystem.authenticated_backend.model.ApplicationUser;
 
+@Repository
 public interface UserRepository extends JpaRepository<ApplicationUser, Integer>{
     Optional<ApplicationUser> findByUsername(String username);
 }
