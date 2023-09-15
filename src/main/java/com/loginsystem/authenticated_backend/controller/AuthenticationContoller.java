9ac66862e819879715f4bf36e82ea5bfd964ecb1
t0nn1x <antoonhrobust@gmail.com>
2023-09-15 18:18:34 +0200

    Created the loginUser method in the AuthController

--- src/main/java/com/loginsystem/authenticated_backend/controller/AuthenticationContoller.java
+++ src/main/java/com/loginsystem/authenticated_backend/controller/AuthenticationContoller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.loginsystem.authenticated_backend.dto.LoginResponseDTO;
 import com.loginsystem.authenticated_backend.dto.RegistrationDTO;
 import com.loginsystem.authenticated_backend.model.ApplicationUser;
 import com.loginsystem.authenticated_backend.service.AuthenticationService;
@@ -22,5 +23,10 @@ public class AuthenticationContoller {
     @PostMapping("/register")
     public ApplicationUser registerUser(@RequestBody RegistrationDTO body){
         return authenticationService.registerUser(body.getUsername(), body.getPassword());
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO loginUser(@RequestBody RegistrationDTO body){
+        return authenticationService.loginUser(body.getUsername(), body.getPassword());
     }
 }
